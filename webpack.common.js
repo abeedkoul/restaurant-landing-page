b6c72cbd68306b0c439235b3677e2184520e6ed5
abeedkoul <koulabeed@gmail.com>
2025-09-27 15:47:47 +0530

    separted the prod and dev modes using webpack merge tool

--- webpack.common.js
+++ webpack.common.js
@@ -4,16 +4,12 @@ const { SDK_VERSION } = require("firebase/app");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
+    
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname,"dist"),
         clean: true
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/home.html"]
     },
     module: {
         rules: [
